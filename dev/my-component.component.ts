8b13a3cb7eb45791cd97c6fb8b21b713927eddca
Angelo Conconi <AlCaponi@Angelos-MacBook-Pro.local>
2016-10-14 21:42:33 +0200

    Added Awesome class adding feature

--- dev/my-component.component.ts
+++ dev/my-component.component.ts
@@ -2,8 +2,13 @@ import {Component} from 'angular2/core';
 @Component({
     selector: 'my-component',
     template: `
-        Hi, I'm {{name}}
+        Hi, I'm {{name}} and this is my very first Angular2 component! <span [class.is-awesome]="inputElement.value === 'yes'">It's so awesome!</span>
+        <br />
+        <br />
+        Is it awesome?
+        <input type="text" #inputElement (keyup)="0">
     `,
+    styleUrls: ['src/css/mycomponent.css']
 })
 export class MyComponentComponent{
     name = "Angelo Conconi!";
