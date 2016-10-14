d2fd2dbc2209061330e8c7ff38fc108be03f086d
Angelo Conconi <AlCaponi@Angelos-MacBook-Pro.local>
2016-10-14 21:32:00 +0200

    Added my Component

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -1,11 +1,13 @@
 import {Component} from 'angular2/core';
-
+import {MyComponentComponent} from "./my-component.component"
 @Component({
     selector: 'my-app',
     template: `
         <h1>Angular 2 Boilerplate</h1>
-        <p>Hello World!</p>
+        <p>Ciao Pfau!</p>
+        <my-component></my-component>
     `,
+    directives: [MyComponentComponent]
 })
 export class AppComponent {
 
---
+++ dev/my-component.component.ts
@@ -0,0 +1,10 @@
+import {Component} from 'angular2/core';
+@Component({
+    selector: 'my-component',
+    template: `
+        Hi, I'm {{name}}
+    `,
+})
+export class MyComponentComponent{
+    name = "Angelo Conconi!";
+}
